7ab3b13fdbc316770a1ce8ebf46102809afd85a7
schoenborn <schoenborn@caa2ac01-1b1e-4351-a100-d44f7e30510d>
2009-10-28 22:49:53 +0000

    fixed some coding issues

--- src/main/java/com/cosmocode/image/CustomImageAnalyzer.java
+++ src/main/java/com/cosmocode/image/CustomImageAnalyzer.java
@@ -7,166 +7,166 @@ import java.io.InputStream;
 
 public class CustomImageAnalyzer implements ImageAnalyzer  {
  
-	// Liste der Segment-Typen
-	private static final int M_SOF0 = 0xC0;
-	private static final int M_SOF1 = 0xC1;
-	private static final int M_SOF2 = 0xC2;
-	private static final int M_SOF3 = 0xC3;
-	private static final int M_SOF5 = 0xC5;
-	private static final int M_SOF6 = 0xC6;
-	private static final int M_SOF7 = 0xC7;
-	private static final int M_SOF9 = 0xC9;
-	private static final int M_SOF10 = 0xCA;
-	private static final int M_SOF11 = 0xCB;
-	private static final int M_SOF13 = 0xCD;
-	private static final int M_SOF14 = 0xCE;
-	private static final int M_SOF15 = 0xCF;
+    // Liste der Segment-Typen
+    private static final int M_SOF0 = 0xC0;
+    private static final int M_SOF1 = 0xC1;
+    private static final int M_SOF2 = 0xC2;
+    private static final int M_SOF3 = 0xC3;
+    private static final int M_SOF5 = 0xC5;
+    private static final int M_SOF6 = 0xC6;
+    private static final int M_SOF7 = 0xC7;
+    private static final int M_SOF9 = 0xC9;
+    private static final int M_SOF10 = 0xCA;
+    private static final int M_SOF11 = 0xCB;
+    private static final int M_SOF13 = 0xCD;
+    private static final int M_SOF14 = 0xCE;
+    private static final int M_SOF15 = 0xCF;
 
-	private final String docroot;
-	
-	/** Erzeugt ein neues Objekt vom CustomImageAnalyzer.
-	 * Bei diesem ImageAnalyzer werden eigene Methoden zum Anladen eines
-	 * images benutzt.
-	 */
-	public CustomImageAnalyzer ( ) {
-		docroot = "";
-	}
+    private final String docroot;
+    
+    /** Erzeugt ein neues Objekt vom CustomImageAnalyzer.
+     * Bei diesem ImageAnalyzer werden eigene Methoden zum Anladen eines
+     * images benutzt.
+     */
+    public CustomImageAnalyzer ( ) {
+        docroot = "";
+    }
 
-	/** Erzeugt ein neues Objekt vom CustomImageAnalyzer.
-	 * Bei diesem ImageAnalyzer werden eigene Methoden zum Anladen eines
-	 * images benutzt. Ebenso wird beim Anladen des Bildes noch ein Pfad
-	 * vorangesetzt.
-	 */
-	public CustomImageAnalyzer(String docroot) {
-		this.docroot = docroot;
-	}
+    /** Erzeugt ein neues Objekt vom CustomImageAnalyzer.
+     * Bei diesem ImageAnalyzer werden eigene Methoden zum Anladen eines
+     * images benutzt. Ebenso wird beim Anladen des Bildes noch ein Pfad
+     * vorangesetzt.
+     */
+    public CustomImageAnalyzer(String docroot) {
+        this.docroot = docroot;
+    }
 
-	/** laed eine datei mit dem CustomAnalyzer an, erzeugt anhand der datei ein
-	 * neues ImageInfo-Objekt und gibt dies zurueck.
-	 * @param url Dateianme
-	 */
-	public ImageInfo analyse( String url ) throws Exception {
-	    ImageInfo ii = gifImageHeader( docroot+url );
-		if (ii==null) ii = jpegImageHeader( docroot+url );
+    /** laed eine datei mit dem CustomAnalyzer an, erzeugt anhand der datei ein
+     * neues ImageInfo-Objekt und gibt dies zurueck.
+     * @param url Dateianme
+     */
+    public ImageInfo analyse( String url ) throws Exception {
+        ImageInfo ii = gifImageHeader( docroot+url );
+        if (ii==null) ii = jpegImageHeader( docroot+url );
 
         if (ii==null) ii = unknownImageHeader( docroot+url) ;
 
-		return ii;
-	}
+        return ii;
+    }
 
-	/** laed eine datei mit dem CustomAnalyzer an, erzeugt anhand der datei ein
-	 * neues ImageInfo-Objekt und gibt dies zurueck.
-	 * @param in InputStream
-	 */
-	public ImageInfo analyse( InputStream in ) throws Exception {
-	    ImageInfo ii = gifImageHeader( in );
-    	if (ii==null) ii = jpegmageHeader( in );
+    /** laed eine datei mit dem CustomAnalyzer an, erzeugt anhand der datei ein
+     * neues ImageInfo-Objekt und gibt dies zurueck.
+     * @param in InputStream
+     */
+    public ImageInfo analyse( InputStream in ) throws Exception {
+        ImageInfo ii = gifImageHeader( in );
+        if (ii==null) ii = jpegmageHeader( in );
 
-		return ii;
-	}
+        return ii;
+    }
 
-	private ImageInfo getImageHeader( String url, long size, DataInputStream dis ) throws Exception {
-		final byte[] headerinfo = new byte[6];
-		final byte[] widthinfo = new byte[2];
-		final byte[] heightinfo = new byte[2];
+    private ImageInfo getImageHeader( String url, long size, DataInputStream dis ) throws Exception {
+        final byte[] headerinfo = new byte[6];
+        final byte[] widthinfo = new byte[2];
+        final byte[] heightinfo = new byte[2];
 
-		dis.read( headerinfo,0,6 );
-		final String signatur = new String(headerinfo,0,3);
-		final String version = new String(headerinfo,3,3);
+        dis.read( headerinfo,0,6 );
+        final String signatur = new String(headerinfo,0,3);
+        final String version = new String(headerinfo,3,3);
 
-		if ( (!signatur.equals("GIF")) && ( (!version.equals("89a")) || (!version.equals("87a")) ) )
-			return null;
+        if ( (!signatur.equals("GIF")) && ( (!version.equals("89a")) || (!version.equals("87a")) ) )
+            return null;
 
         dis.read( widthinfo, 0, 2);
         dis.read( heightinfo, 0, 2);
-		
+        
         final int width = ( widthinfo[0] & 0xFF) | ( widthinfo[1] << 8);
         final int height = ( heightinfo[0] & 0xFF) | ( heightinfo[1] << 8);
 
         System.err.println( "IMAGE-BREITE: " + width );
         System.err.println( "IMAGE-H�HE: " + height );
 
-		return new ImageInfo(url, width, height, size );
-	}
-	private ImageInfo getJPEGHeader( String url, long size, DataInputStream dis ) throws Exception {
-		boolean weiter = true;
-		int b;
-		int width=0;
-		int height=0;
+        return new ImageInfo(url, width, height, size );
+    }
+    private ImageInfo getJPEGHeader( String url, long size, DataInputStream dis ) throws Exception {
+        boolean weiter = true;
+        int b;
+        int width=0;
+        int height=0;
 
-		final int header1 = dis.readUnsignedByte();    // header: 0xFF
-		final int header2 = dis.readUnsignedByte();    // header: 0xD8
+        final int header1 = dis.readUnsignedByte();    // header: 0xFF
+        final int header2 = dis.readUnsignedByte();    // header: 0xD8
 
-		if ((header1 != 0xFF) && (header2 != 0xD8)) return null;
+        if ((header1 != 0xFF) && (header2 != 0xD8)) return null;
 
-		do {
-			b = dis.readUnsignedByte(); // Segment-Start: 0xFF
-			b = dis.readUnsignedByte(); // Segment-Type (siehe oben)
+        do {
+            b = dis.readUnsignedByte(); // Segment-Start: 0xFF
+            b = dis.readUnsignedByte(); // Segment-Type (siehe oben)
 
-			switch ( b ) {
-				case M_SOF0:
-				case M_SOF1:
-				case M_SOF2:
-				case M_SOF3:
-				case M_SOF5:
-				case M_SOF6:
-				case M_SOF7:
-				case M_SOF9:
-				case M_SOF10:
-				case M_SOF11:
-				case M_SOF13:
-				case M_SOF14:
-				case M_SOF15:
-					b = dis.readUnsignedShort() - 2;    // Segment-Length
-					dis.readUnsignedByte();         	// data precision
-					height = dis.readUnsignedShort();
-					width = dis.readUnsignedShort();
-					weiter = false;
-					break;
-				default:
-					b = dis.readUnsignedShort() - 2;    // Segment-Length
-					dis.skipBytes(b);
-					break;
-			}
-		} while (weiter);
+            switch ( b ) {
+                case M_SOF0:
+                case M_SOF1:
+                case M_SOF2:
+                case M_SOF3:
+                case M_SOF5:
+                case M_SOF6:
+                case M_SOF7:
+                case M_SOF9:
+                case M_SOF10:
+                case M_SOF11:
+                case M_SOF13:
+                case M_SOF14:
+                case M_SOF15:
+                    b = dis.readUnsignedShort() - 2;    // Segment-Length
+                    dis.readUnsignedByte();             // data precision
+                    height = dis.readUnsignedShort();
+                    width = dis.readUnsignedShort();
+                    weiter = false;
+                    break;
+                default:
+                    b = dis.readUnsignedShort() - 2;    // Segment-Length
+                    dis.skipBytes(b);
+                    break;
+            }
+        } while (weiter);
 
-		return new ImageInfo(url, width, height, size );
-	}
-	private ImageInfo gifImageHeader( InputStream in ) throws Exception {
-		final DataInputStream dis = new DataInputStream( in );
-		return getImageHeader( "", 0L, dis ) ;
-	}
+        return new ImageInfo(url, width, height, size );
+    }
+    private ImageInfo gifImageHeader( InputStream in ) throws Exception {
+        final DataInputStream dis = new DataInputStream( in );
+        return getImageHeader( "", 0L, dis ) ;
+    }
 
 
-	/** gibt ein ImageInfo-Objekt von einer GIF-datei zurueck. sollte es
-	 * sich nicht um eine GIF-Datei handeln, wird ein NULL zurueck
-	 * gegeben.
-	 */
-	private ImageInfo gifImageHeader( String url  ) throws Exception {
-		final File f = new File( url );
-		final FileInputStream in = new FileInputStream( url );
-		final DataInputStream dis = new DataInputStream( in );
-		return getImageHeader( url, f.length() , dis ) ;
-	}
+    /** gibt ein ImageInfo-Objekt von einer GIF-datei zurueck. sollte es
+     * sich nicht um eine GIF-Datei handeln, wird ein NULL zurueck
+     * gegeben.
+     */
+    private ImageInfo gifImageHeader( String url  ) throws Exception {
+        final File f = new File( url );
+        final FileInputStream in = new FileInputStream( url );
+        final DataInputStream dis = new DataInputStream( in );
+        return getImageHeader( url, f.length() , dis ) ;
+    }
 
-	/** gibt ein ImageInfo-Objekt von einer JPEG-datei zurueck. sollte es
-	 * sich nicht um eine JPEG-Datei handeln, wird ein NULL zurueck
-	 * gegeben.
-	 */
-	private ImageInfo jpegImageHeader( String url ) throws Exception {
-	    final File f = new File( url );
-	    final FileInputStream in = new FileInputStream( url );
-	    final DataInputStream dis = new DataInputStream( in );
-		return getJPEGHeader( url, f.length() , dis ) ;
-	}
-	private ImageInfo unknownImageHeader( String url  ) throws Exception {
-	    final File f = new File( url );
-		return new ImageInfo( url, -1, -1, f.length() ) ;
-	}
-	private ImageInfo jpegmageHeader( InputStream in ) throws Exception {
-	    final DataInputStream dis = new DataInputStream( in );
-		return getJPEGHeader( "", 0L , dis ) ;
-	}
+    /** gibt ein ImageInfo-Objekt von einer JPEG-datei zurueck. sollte es
+     * sich nicht um eine JPEG-Datei handeln, wird ein NULL zurueck
+     * gegeben.
+     */
+    private ImageInfo jpegImageHeader( String url ) throws Exception {
+        final File f = new File( url );
+        final FileInputStream in = new FileInputStream( url );
+        final DataInputStream dis = new DataInputStream( in );
+        return getJPEGHeader( url, f.length() , dis ) ;
+    }
+    private ImageInfo unknownImageHeader( String url  ) throws Exception {
+        final File f = new File( url );
+        return new ImageInfo( url, -1, -1, f.length() ) ;
+    }
+    private ImageInfo jpegmageHeader( InputStream in ) throws Exception {
+        final DataInputStream dis = new DataInputStream( in );
+        return getJPEGHeader( "", 0L , dis ) ;
+    }
  
  
 } // end class CustomImageAnalyzer
